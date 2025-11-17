6b74d247fab5e094dfb6250d57c0d8be1a2d7f55
Dawson Toth <dawson@harperdb.io>
2025-11-17 14:59:27 -0500

    Rename HDBMS to Studio

--- utility/devops/build/build-studio.sh
+++ utility/devops/build/build-studio.sh
@@ -3,7 +3,7 @@
 rm -Rf studio
 mkdir studio
 cd studio
-git clone --branch prod --single-branch --depth 1 https://github.com/HarperDB/hdbms.git .
+git clone --branch prod --single-branch --depth 1 https://github.com/HarperFast/studio.git .
 npm install -g pnpm
 pnpm install
 VITE_STUDIO_VERSION="v$(jq -r '.version' ../package.json)" pnpm run build:local
