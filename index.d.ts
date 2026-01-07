bcf2c011d93c14f552d0e397714c14704658c920
Dawson Toth <dawson@harperdb.io>
2026-01-07 12:52:33 -0500

    Make v2 the default type export

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
-export { Resource } from './resources/Resource.ts';
-import { Resource as ResourceImport } from './resources/Resource.ts';
+export { ResourceV2 as Resource } from './resources/ResourceV2.ts';
+import { ResourceV2 as ResourceImport } from './resources/ResourceV2.ts';
 export type {
-	ResourceInterface,
 	Query,
 	Context,
 	SubscriptionRequest,
 	RequestTargetOrId,
 } from './resources/ResourceInterface.ts';
+export { ResourceInterfaceV2 as ResourceInterface } from './resources/ResourceInterfaceV2.ts';
 export type { User } from './security/user.ts';
 export type { RecordObject } from './resources/RecordEncoder.ts';
 export { RequestTarget } from './resources/RequestTarget.ts';
--- v2.d.ts
+++ v2.d.ts
@@ -1,4 +1,1 @@
 export * from './index.d.ts';
-
-export { ResourceV2 as Resource } from './resources/ResourceV2.ts';
-export { ResourceInterfaceV2 as ResourceInterface } from './resources/ResourceInterfaceV2.ts';
