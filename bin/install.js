ca04602819b57114ca11f93b6eb23ff028a080ec
Kris Zyp <kriszyp@gmail.com>
2026-03-11 09:51:52 -0600

    Upgrade @harperfast/rocksdb-js and remove unnecessary exit call

--- bin/install.js
+++ bin/install.js
@@ -6,7 +6,6 @@ module.exports = install;
 async function install() {
 	try {
 		await installer.install();
-		process.exit(0);
 	} catch (err) {
 		console.error('There was an error during the install.');
 		console.error(err);
