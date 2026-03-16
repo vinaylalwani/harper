ab2d40b921321e6f352353a7f4cb4ecf52a5bbc6
Kris Zyp <kriszyp@gmail.com>
2026-03-15 19:00:46 -0600

    Update loader to properly handle circular dependencies

--- unitTests/components/fixtures/testJSWithDeps/child-dir/in-child-dir.js
+++ unitTests/components/fixtures/testJSWithDeps/child-dir/in-child-dir.js
@@ -3,8 +3,10 @@ import { Resource } from 'harperdb';
 import { connect } from 'mqtt'; // verify we can import from node_modules packages
 import 'micromatch';
 import 'needle';
+import { testCircularExport } from './circular.js';
 // TODO: Verify/support circular dependencies
 console.log('Verifying we can access console.log in transitive module in application');
+assert(testCircularExport);
 // verify we can't access parent global variables
 assert(typeof globalVariableFromParent, 'undefined', 'Global variable from parent value should not be present');
 
