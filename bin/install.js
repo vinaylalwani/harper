5205998baf009eeaee6475bf32542334357338d0
Chris Barber <chris@harperdb.io>
2026-03-17 13:51:35 -0500

    Exit after install

--- bin/install.js
+++ bin/install.js
@@ -6,6 +6,9 @@ module.exports = install;
 async function install() {
 	try {
 		await installer.install();
+
+		// TODO: Remove this once rocksdb-js 0.1.12 is released
+		process.exit(0);
 	} catch (err) {
 		console.error('There was an error during the install.');
 		console.error(err);
